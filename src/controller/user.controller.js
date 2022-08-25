2d85f6aa536e7af10ee649ae2600043ad6698f74
苏凛夜 <2625070801@qq.com>
2022-08-25 10:22:54 +0800

    11-错误处理的补充与完善

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,6 @@
 const sequelize = require('sequelize')
-const {createUser, getUerInfo}=require('../service/user.service')
+const {createUser}=require('../service/user.service')
+const {userRegisterError}=require('../constant/err.type')
 
 class UserController{
     async register(ctx,next){
@@ -7,39 +8,25 @@ class UserController{
         // console.log(ctx.request.body)
         const {user_name,password}=ctx.request.body
 
-        //合法性
-        if(!user_name||!password){
-            console.error('用户名或密码为空',ctx.request.body)
-            ctx.status=400,
-            ctx.body={
-            code:'10001',
-            message:'用户名或密码为空',
-            result:'',
-            }
-            return 
-        }
-        //合理性
-         if(getUerInfo({user_name})){
-            ctx.status=409
-            ctx.body={
-                code:'10002',
-                message:'用户已经存在',
-                result:'',
-            }
-            return
-         }
         //2. 操作数据库
-         const res=await createUser(user_name,password)
-         console.log(res)
-        //3.返回结果
-        ctx.body={
-            code:0,
-            message:'用户注册成功',
-            result:{
-                id:res.id,
-                user_name:res.user_name
-            },
+        try{
+            const res=await createUser(user_name,password)
+            console.log(res)
+           //3.返回结果
+           ctx.body={
+               code:0,
+               message:'用户注册成功',
+               result:{
+                   id:res.id,
+                   user_name:res.user_name
+               },
+           }
+        }catch(err){
+            console.log(err)
+            ctx.app.emit('error',userRegisterError,ctx)
         }
+
+       
         
     }
     async login(ctx,next){ 
--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,9 +1,44 @@
+const{getUerInfo}=require('../service/user.service')
+const{userFormateError,userAlreadyExited,userRegisterError}=require('../constant/err.type')
+const uservalidator=async (ctx,next)=>{
+  const{user_name,password}=ctx.request.body
+     //合法性
+    if(!user_name||!password){
+        console.error('用户名或密码为空',ctx.request.body)
+       ctx.app.emit('error',userFormateError,ctx)
+        return 
+    }
+    await next()
+}
 
-const uservalidator=(ctx,next)=>{
-     
+
+
+const verifyUser=async (ctx,next)=>{
+     const{user_name}=ctx.request.body 
+    //合理性
+    //   if(await getUerInfo({user_name})){
+    //    ctx.app.emit('error',userAlreadyExited,ctx)
+    //     return
+    //  }
+    try{
+      const res =await getUerInfo({user_name})
+      if(res){
+        console.error('用户名已经存在',{user_name})
+        ctx.app.emit('error',userAlreadyExited.ctx)
+        return
+      }
+    }catch(err){
+        console.log('获取用户信息错误',err)
+        ctx.app.emit('error',userRegisterError,ctx)
+        return
+    }
+
+
+await next()
 }
 
 
 module.exports={
     uservalidator,
+    verifyUser,
 }
